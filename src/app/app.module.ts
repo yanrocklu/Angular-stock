8be9dc347a1636b5af52117a6ee2dc5de32fbaeb
Yan Lu <rocklyan@gmail.com>
2017-10-29 00:18:29 -0400

    craete a feature mofule, auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { StocksComponent } from './stocks/stocks.component';
 import {AppRoutingModule} from "./app-routing/app-routing.module";
-import { StockListComponent } from './stocks/stock-list/stock-list.component';
-import { StockStartComponent } from './stocks/stock-start/stock-start.component';
 import {StockService} from "./stocks/stock.service";
-import { StockItemComponent } from './stocks/stock-list/stock-item/stock-item.component';
-import { StockDetailComponent } from './stocks/stock-detail/stock-detail.component';
-import { StockEditComponent } from './stocks/stock-edit/stock-edit.component';
 import {DropdownDirective} from "./shared/dropdown.directive";
-import {PipesModule} from "./pipe/pipes.module";
 import {CommonModule} from "@angular/common";
 import { ErrorPageComponent } from './error-page/error-page.component';
 import {DataStorageService} from "./shared/data-storage.service";
 import { SaveDataComponent } from './popup/save-data/save-data.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import {AuthService} from "./auth/auth.service";
 import {StocksModule} from "./stocks/stocks.module";
+import {SharedModule} from "./shared/shared.module";
+import {AuthModule} from "./auth/auth.module";
 // import {Ng2PopupModule} from "ng2-popup";
 
 @NgModule({
@@ -31,11 +24,8 @@ import {StocksModule} from "./stocks/stocks.module";
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    DropdownDirective,
     ErrorPageComponent,
-    SaveDataComponent,
-    SigninComponent,
-    SignupComponent
+    SaveDataComponent
   ],
   imports: [
     BrowserModule,
@@ -43,7 +33,9 @@ import {StocksModule} from "./stocks/stocks.module";
     HttpModule,
     AppRoutingModule,
     StocksModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    AuthModule
     // Ng2PopupModule
   ],
   exports: [AppRoutingModule],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {DropdownDirective} from "./dropdown.directive";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    DropdownDirective
+  ]
+})
+export class SharedModule {
+}
